b548d68534dd6f0a01a4073ad4c018856b1c1c3a
Jonathan González T <132779706+JonathanGT23@users.noreply.github.com>
2023-10-06 17:24:40 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,83 @@
 import React from 'react'
 import { styled } from 'styled-components'
 import Logo from './Logo'
+import {SiLinkedin} from "react-icons/si"
+import {FaSquareInstagram} from "react-icons/fa6"
+import {AiFillFacebook,AiFillGithub} from "react-icons/ai"
 
 const StyleFooter = styled.footer`
     width: 100%;
     display: flex;
-    align-items: center;
-    justify-content: center;
+  
     padding: 20px  40px;
     border-top: 3px solid var(--color-tercero);
     margin-top: auto;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+
+
+
+.containerLink{
+  display: flex;
+  justify-content: flex-start;
+gap: 1rem;
+}
+
+
+.containerInfo{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1.2rem;
+  span{
+    color: var(--color-tercero);
+  }
+}
+
+
+.containerLogo{
+  display: flex;
+  justify-content: center;
+}
+
+
+`
+
+const Links = styled.a`
+
+    text-decoration: none;
+    color: var(--color-tercero);
+    font-size: 35px;
+
+    :hover{
+      color: white;
+    }
+
 `
 
 function Footer() {
   return (
     <StyleFooter >
-        <Logo/>
+        
+        
+        <div className='containerLink'>
+          <Links href="https://www.linkedin.com/in/jonathan-gonzalez-t-007a52290/" target='_blank'><SiLinkedin/></Links>
+          <Links href="https://www.instagram.com/jonathan_gonzalezt/" target='_blank'><FaSquareInstagram/></Links>
+          <Links href="https://www.facebook.com/profile.php?id=100088565150787" target='_blank'><AiFillFacebook/></Links>
+          <Links href="https://github.com/JonathanGT23" target='_blank'><AiFillGithub/></Links>
+        </div>
+
+        <div className='containerLogo'>
+          <Logo />
+        </div>
+
+        <div className='containerInfo'>
+           <p>Desarrollado por Jonathan González T</p>
+          <span>&copy; {new Date().getFullYear()}</span>
+        </div>
+       
     </StyleFooter>
   )
 }
